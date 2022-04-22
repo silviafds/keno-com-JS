c357bdd298eb5acdcbbe1a935d6e0185b154ec5a
silviafds <silviafernandes10@outlook.com.br>
2022-04-22 11:27:59 -0300

    novas funções JS adicionadas

--- src/validarDados.js
+++ src/validarDados.js
@@ -1,16 +1,19 @@
+var verificacaoValorAposta = 0, verificacaoRodadas = 0, verificacaoNumerosApostados = 0;
+
 function validaValorApostado() {
     var valorApostado = document.getElementById("valorApostar").value;
 
     if(valorApostado <= 0) {
         document.getElementById("valorApostar").style.borderColor = "red";
-        document.getElementById("respostaValorAposta").style.color = "red";
-        document.getElementById("respostaValorAposta").innerHTML = "Informe um valor maior que 0."
+        document.getElementById("iconInvalidacao").style.display = "flex";
+        document.getElementById("iconValidacao").style.display = "none";
         return false;
     }
 
     document.getElementById("valorApostar").style.borderColor = "green";
-    document.getElementById("respostaValorAposta").style.color = "green";
-    document.getElementById("respostaValorAposta").innerHTML = "Valor informado válido.";
+    document.getElementById("iconValidacao").style.display = "flex";
+    document.getElementById("iconInvalidacao").style.display = "none";
+    verificacaoValorAposta = 1;
     return true;
 }
 
@@ -18,15 +21,16 @@ function validaNumRodadas() {
     var recebeQtdApostas = document.getElementById("numeroRodadas").value;
 
     if(recebeQtdApostas < 1) {
-        document.getElementById("respostasNumeroRodadas").style.borberColor = "red";
-        document.getElementById("respostaNumeroRodadas").style.color = "red";
-        document.getElementById("resostaNumeroRodadas").innerHTML = "O número mńimo de jogadas é 1";
+        document.getElementById("numeroRodadas").style.borderColor = "red";
+        document.getElementById("iconInvalidacaoRodadas").style.display = "flex";
+        document.getElementById("iconValidacaoRodadas").style.display = "none";
         return false;
     }
 
-    document.getElementById("respostasNumeroRodadas").style.borberColor = "green";
-    document.getElementById("respostaNumeroRodadas").style.color = "green";
-    document.getElementById("resostaNumeroRodadas").innerHTML = "Informação válida";
+    document.getElementById("numeroRodadas").style.borderColor = "green";
+    document.getElementById("iconInvalidacaoRodadas").style.display = "none";
+    document.getElementById("iconValidacaoRodadas").style.display = "flex";
+    verificacaoRodadas = 1;
     return true;
 }
 
@@ -36,16 +40,22 @@ function validaNumApostados() {
     var novoVetor = Array.from(xx); //transforma em novo array
     
     var vetorNumerosOrdenados = ordenaNumeros(novoVetor);
-    var recebeValorRepeticao = buscaNumerosRepetidos(vetorNumerosOrdenados);
+    var recebeValorRepeticao = buscaNumerosRepetidos(vetorNumerosOrdenados); //retorna valor booleano
     var recebeQtdNumerosApostados = validaQuantidadeNumerosApostado(vetorNumerosOrdenados);
 
-    if(50 > 6) {
+    if(recebeValorRepeticao == false && recebeQtdNumerosApostados == 0) {
+        document.getElementById("numeroApostado").style.borderColor = "green";
+        document.getElementById("iconValidacaoNumeros").style.display = "flex"
+        document.getElementById("iconInvalidacaoNumeros").style.display = "none"
+        verificacaoNumerosApostados = 1;
+        return true;
+    } else {
         document.getElementById("numeroApostado").style.borderColor = "red";
-        document.getElementById("respostaNumApostados").innerHTML = recebeQtdNumerosApostados;
-        
+        document.getElementById("iconValidacaoNumeros").style.display = "none"
+        document.getElementById("iconInvalidacaoNumeros").style.display = "flex";
         return false;
     }
-    return true;
+
 }
 
 /* Função na qual coloca os números apostados em ordem crescente 
@@ -76,4 +86,14 @@ function validaQuantidadeNumerosApostado(vetorNumerosOrdenados_) {
         return 1;
     }
     return 0;
+}
+
+function validaDadosFornecidos() {
+    var resultadoVerificacao = verificacaoValorAposta + verificacaoRodadas + verificacaoNumerosApostados;
+
+    if(resultadoVerificacao == 3) {
+        alert("Dados preenchidos com sucesso. Clique em 'OK' para iniciar jogo");
+    } else {
+        alert("Dados fornecidos não estão totalmente corretos.");
+    }
 }
