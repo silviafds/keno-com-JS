9d83ab5c051c93082bb903d252793dae015e3c42
silviafds <silviafernandes10@outlook.com.br>
2022-04-30 21:12:41 -0300

    versao codigo pagina unica

--- src/validarDados.js
+++ src/validarDados.js
@@ -1,6 +1,7 @@
-var verificacaoValorAposta = 0, verificacaoRodadas = 0, verificacaoNumerosApostados = 0;
-var PegaValorApostado, PegaQtdNumerosApostados, numApostados = [], PegaValorRodada;
+var verificacaoValorAposta = 0, verificacaoNumerosApostados = 0;
+var PegaValorApostado, PegaQtdNumerosApostados, vetorNumerosJogados = [];
 
+// função valida o valor apostado pelo jogador
 function validaValorApostado() {
     var valorApostado = document.getElementById("valorApostar").value;
     
@@ -21,24 +22,7 @@ function validaValorApostado() {
     return true;
 }
 
-function validaNumRodadas() {
-    var recebeQtdApostas = document.getElementById("numeroRodadas").value;
-    
-    if(recebeQtdApostas < 1) {
-        document.getElementById("numeroRodadas").style.borderColor = "red";
-        document.getElementById("iconInvalidacaoRodadas").style.display = "flex";
-        document.getElementById("iconValidacaoRodadas").style.display = "none";
-        return false;
-    }
-    PegaValorRodada = recebeQtdApostas;
-    document.getElementById("numeroRodadas").style.border = "solid";
-    document.getElementById("numeroRodadas").style.borderColor = "green";
-    document.getElementById("iconInvalidacaoRodadas").style.display = "none";
-    document.getElementById("iconValidacaoRodadas").style.display = "flex";
-    verificacaoRodadas = 1;
-    return true;
-}
-
+// função valida os numeros apostados pela jogador
 function validaNumApostados() {
     var recebeNumerosApostados = document.getElementById("numeroApostado").value;
     var xx = recebeNumerosApostados.split(" "); //retira espaços desnecessários
@@ -47,6 +31,11 @@ function validaNumApostados() {
     var vetorNumerosOrdenados = ordenaNumeros(novoVetor);
     var recebeValorRepeticao = buscaNumerosRepetidos(vetorNumerosOrdenados); //retorna valor booleano
     var recebeQtdNumerosApostados = validaQuantidadeNumerosApostado(vetorNumerosOrdenados);
+
+    //preenche vetor com numeros do jogador
+    for(var i = 0; i < vetorNumerosOrdenados.length; i++) {
+        vetorNumerosJogados[i] = vetorNumerosOrdenados[i];
+    }
 
     if(recebeValorRepeticao == false && recebeQtdNumerosApostados == 0) {
         document.getElementById("numeroApostado").style.border = "solid";
@@ -63,7 +52,7 @@ function validaNumApostados() {
     }
 
 }
-var vetorzinho = [];
+
 /* Função na qual coloca os números apostados em ordem crescente 
     @vetorOrdenado vetor com os números em ordem crescente
 */
@@ -77,13 +66,7 @@ function ordenaNumeros(vetorNumerosDesordenados_) {
     vetorOrdenado.sort(function (a, b) { //números ficam em ordem crescente
 	    return (a > b) ? 1 : ((b > a) ? -1 : 0);
     });
-    numApostados = vetorOrdenado;
 
-    for(var i = 0; i < vetorOrdenado.length; i++) { //converte string para int
-        vetorzinho[i] = vetorOrdenado[i];
-    }
-
-    
     return vetorOrdenado;
 }
 
@@ -104,11 +87,12 @@ function validaQuantidadeNumerosApostado(vetorNumerosOrdenados_) {
 /* função que mostra o valor real da aposta do jogador, apenas se ele digitou
 os valores corretos, caso contrário, o jogo não continua*/ 
 function validaDadosFornecidos() {
-    var resultadoVerificacao = verificacaoValorAposta + verificacaoRodadas + verificacaoNumerosApostados;
+    var resultadoVerificacao = verificacaoValorAposta + verificacaoNumerosApostados;
 
-    if(resultadoVerificacao == 3) {
-        alert("Aposta lida com sucesso! \nVocê apostará $:" + PegaValorApostado + "." + "\nJogará " + PegaValorRodada + " rodadas, apostando $" + valorApostaPorRodada() + " créditos por rodada." + "\nSua aposta tem " + PegaQtdNumerosApostados + " números, eles são " + numApostados + "\n Clique em 'OK' para iniciar o jogo.");
-        window.location.assign("introducaoAposta.html");
+    if(resultadoVerificacao == 2) {
+        alert("Aposta lida com sucesso! \nVocê apostará $:" + PegaValorApostado + "." + "\nJogará 3 rodadas, apostando $" + valorApostaPorRodada() + " créditos por rodada." + "\nSua aposta tem " + PegaQtdNumerosApostados + " números, eles são " + vetorNumerosJogados + "\n Clique em 'OK' para iniciar o jogo.");
+        //window.location.assign("introducaoAposta.html");
+        preencheBolasPrimaria();
     } else {
         alert("Dados fornecidos não estão totalmente corretos.");
     }
@@ -116,47 +100,114 @@ function validaDadosFornecidos() {
 
 /* função que calcula o valor que sera apostado em cada rodada*/
 function valorApostaPorRodada() {
-    var valorPorRodada = PegaValorApostado/PegaValorRodada;
+    var valorPorRodada = PegaValorApostado/3;
     return valorPorRodada.toFixed(2);
 }
 
-/* Essa parte corresponde ao arquivo introducaoApostas.html*/ 
+/* Essa parte corresponde ao inicio do jogo*/ 
 
-var contadorDeRodadas = 1; // variavel q atualiza valor a cada rodada
-
-//função informa qual rodada atual
-function bannerRodadas() {
-    document.getElementById("informaRodadaAtual").innerHTML = contadorDeRodadas + 'º Rodada'
-    setInterval(preencheBolasPrimaria, 2000);
+//função que chama a proxima rodada, inicia na segunda rodada
+function proximaRodada() {
+    //setInterval(preencheBolasPrimaria, 2000);
 }
 
+// função que sorteia numeros
+var listaNumerosSorteados = [];
+function sorteiaBolas() {
+    var listaOrdenada = [];
+
+    //gera numeros ordenados
+    for(var i = 0; i < 30; i++) {
+        listaOrdenada[i] = i + 1;
+    }
+    var tmp, n;
+    //embaralha numeros
+    for(var p = listaOrdenada.length; p;) {
+        n = Math.random() * p-- | 0;
+        tmp = listaOrdenada[n];
+        listaOrdenada[n] = listaOrdenada[p];
+        listaOrdenada[p] = tmp;
+    }
+
+    //preenche com os primeiros 20 numeros embaralhados
+    for(var l = 0; l < 20; l++) {
+        listaNumerosSorteados[l] = listaOrdenada[l];
+    }
+    return true;
+}
+
+// função que preenche as bolas da primeira rodada
 function preencheBolasPrimaria() {
-   
-    document.getElementById("n1").innerHTML = PegaValorApostado;
-    /*document.getElementById("n2").innerHTML = numApostados[1];
-    document.getElementById("n3").innerHTML = numApostados[2];
-    document.getElementById("n4").innerHTML = numApostados[3];
-    document.getElementById("n5").innerHTML = numApostados[4];
-    document.getElementById("n6").innerHTML = numApostados[5];
-    document.getElementById("n7").innerHTML = numApostados[6];
-    document.getElementById("n8").innerHTML = numApostados[7];
-    document.getElementById("n9").innerHTML = numApostados[8];
-    document.getElementById("n10").innerHTML = numApostados[9];*/
+    sorteiaBolas();
+    listaNumerosSorteados = ordenaNumeros(listaNumerosSorteados);
+    document.getElementById("n1").innerHTML = listaNumerosSorteados[0];
+    document.getElementById("n2").innerHTML = listaNumerosSorteados[1];
+    document.getElementById("n3").innerHTML = listaNumerosSorteados[2];
+    document.getElementById("n4").innerHTML = listaNumerosSorteados[3];
+    document.getElementById("n5").innerHTML = listaNumerosSorteados[4];
+    document.getElementById("n6").innerHTML = listaNumerosSorteados[5];
+    document.getElementById("n7").innerHTML = listaNumerosSorteados[6];
+    document.getElementById("n8").innerHTML = listaNumerosSorteados[7];
+    document.getElementById("n9").innerHTML = listaNumerosSorteados[8];
+    document.getElementById("n10").innerHTML = listaNumerosSorteados[9];
     setInterval(preencheBolasSecudaria, 1000);
-
 }
-
+// função que preenche as bolas da primeira rodada
 function preencheBolasSecudaria() {
-    document.getElementById("n11").innerHTML = 8;
-    document.getElementById("n12").innerHTML = 18;
-    document.getElementById("n13").innerHTML = 3;
-    document.getElementById("n14").innerHTML = 16;
-    document.getElementById("n15").innerHTML = 25;
-    document.getElementById("n16").innerHTML = 8;
-    document.getElementById("n17").innerHTML = 18;
-    document.getElementById("n18").innerHTML = 3;
-    document.getElementById("n19").innerHTML = 16;
-    document.getElementById("n20").innerHTML = 25;
-    
+    document.getElementById("n11").innerHTML = listaNumerosSorteados[10];
+    document.getElementById("n12").innerHTML = listaNumerosSorteados[11];
+    document.getElementById("n13").innerHTML = listaNumerosSorteados[12];
+    document.getElementById("n14").innerHTML = listaNumerosSorteados[13];
+    document.getElementById("n15").innerHTML = listaNumerosSorteados[14];
+    document.getElementById("n16").innerHTML = listaNumerosSorteados[15];
+    document.getElementById("n17").innerHTML = listaNumerosSorteados[16];
+    document.getElementById("n18").innerHTML = listaNumerosSorteados[17];
+    document.getElementById("n19").innerHTML = listaNumerosSorteados[18];
+    document.getElementById("n20").innerHTML = listaNumerosSorteados[19];
+    comparaBolas();
+    preencheBolasAcertadasR1();
 }
 
+var numerosAcertados = [];
+// função compara bolas para descobrir acertos do jogador
+function comparaBolas() {
+    vetorNumerosJogados.sort(function (a, b) { //números ficam em ordem crescente
+	    return (a > b) ? 1 : ((b > a) ? -1 : 0);
+    });
+    
+    var x = 0;
+    for(var i=0; i<20; i++){
+        for(var j=0; j<vetorNumerosJogados.length; j++){
+            if(listaNumerosSorteados[i] == vetorNumerosJogados[j]){
+                numerosAcertados[x] = vetorNumerosJogados[j];
+                x++;
+            }
+        }
+    }
+    // preenche valores vazios com 'x'
+    for(var j=0; j<=15; j++){
+        if(numerosAcertados[j] == undefined){
+            numerosAcertados[j] = "x";
+        }
+    }
+
+}
+
+function preencheBolasAcertadasR1() {
+    document.getElementById("a1-r1").innerHTML = numerosAcertados[0];
+    document.getElementById("a2-r1").innerHTML = numerosAcertados[1];
+    document.getElementById("a3-r1").innerHTML = numerosAcertados[2];
+    document.getElementById("a4-r1").innerHTML = numerosAcertados[3];
+    document.getElementById("a5-r1").innerHTML = numerosAcertados[4];
+    document.getElementById("a6-r1").innerHTML = numerosAcertados[5];
+    document.getElementById("a7-r1").innerHTML = numerosAcertados[6];
+    document.getElementById("a8-r1").innerHTML = numerosAcertados[7];
+    document.getElementById("a8-r1").innerHTML = numerosAcertados[8];
+    document.getElementById("a9-r1").innerHTML = numerosAcertados[9];
+    document.getElementById("a10-r1").innerHTML = numerosAcertados[10];
+    document.getElementById("a11-r1").innerHTML = numerosAcertados[11];
+    document.getElementById("a12-r1").innerHTML = numerosAcertados[12];
+    document.getElementById("a13-r1").innerHTML = numerosAcertados[13];
+    document.getElementById("a14-r1").innerHTML = numerosAcertados[14];
+    document.getElementById("a15-r1").innerHTML = numerosAcertados[15];
+}
